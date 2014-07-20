8897eb90fed3be13a2e6bdbce4f2c74717095a36
flaviut <tamasflaviu@gmail.com>
2014-07-20 14:59:16 -0400

    Modify according to #1386

--- murmurhash3.nim
+++ murmurhash3.nim
@@ -1,29 +1,20 @@
 import bitutils, unsigned
 
-## An implmentation of the 32-bit MurmurHash algorithm. This is equvilent
-## to the offical implmentation, but is factored into methods to be easy
+## An implementation of the 32-bit MurmurHash algorithm. This is equivalent
+## to the official implementation, but is factored into methods to be easy
 ## to use for hashing non-strings
 
 type
-  THash = uint32
+  IntermediateHash = uint32
 
 const
   c1: uint32 = 0xCC9E2D51'u32
   c2: uint32 = 0x1B873593'u32
 
-proc mixLast*(hash: THash, val: int32): THash =
-  ## A bit faster than mix, and can be used for the last element because it
-  ## doesn't mix the bits up as finalization does that anyways
-  var k = val.uint32
+proc initHash(key: int32): IntermediateHash =
+  result = IntermediateHash(key)
 
-  k *= c1
-  k  = k.rotl(15)
-  k *= c2
-
-  result = hash
-  result = result xor k
-
-proc mix*(hash: THash, val: int32): THash =
+proc mix(hash: IntermediateHash, val: int32): IntermediateHash =
   var k = val.uint32
   result = hash
   
@@ -35,10 +26,22 @@ proc mix*(hash: THash, val: int32): THash =
   result = result.rotl(13)
   result = result * 5'u32 + 0xE6546B64'u32
 
-proc finalize*(hash: THash, len: Natural): THash =
-  result = hash
+proc mixLast(hash: IntermediateHash, val: int32, len: int): IntermediateHash =
+  ## A bit faster than mix, and should be used to
+  ## mix in the last element
+  var k = val.uint32
 
-  result = result xor THash(len)
+  k *= c1
+  k  = k.rotl(15)
+  k *= c2
+
+  result = hash
+  result = result xor k
+
+  result = result xor len.IntermediateHash
+
+proc finalize(hash: IntermediateHash): IntermediateHash =
+  result = hash
 
   result  = result xor (result shr 16)
   result *= 0x85EBCA6B'u32
@@ -46,14 +49,8 @@ proc finalize*(hash: THash, len: Natural): THash =
   result *= 0xC2B2AE35'u32
   result  = result xor (result shr 16)
 
-proc `!&`*(hash: THash, val: int32): THash =
-  mix(hash, val)
-
-proc `!$`*(hash: THash, len: Natural): THash =
-  finalize(hash, len)
-
-proc hash*(data: string, seed: THash): THash =
-  result = seed
+proc hash*(data: string, seed: int32): IntermediateHash =
+  result = initHash(seed)
   let blocks = data.len div 4
 
   block body:
@@ -74,28 +71,30 @@ proc hash*(data: string, seed: THash): THash =
     if extraBytes >= 3: nextBlock = nextBlock xor (int32 ord(data[lastElem+2]) shl 16)
     if extraBytes >= 2: nextBlock = nextBlock xor (int32 ord(data[lastElem+1]) shl 8 )
     if extraBytes >= 1: nextBlock = nextBlock xor (int32 ord(data[lastElem])         )
-    result = result.mixLast(nextBlock)
+    result = result.mixLast(nextBlock, data.len)
 
-  result = result.finalize(data.len)
+  result = result.finalize()
 
-proc hash*[T: int64|uint32](data: T, seed: THash = 0): THash =
-  result = seed
+proc hash*[T: int64|uint32](data: T, seed: int32): IntermediateHash =
+  result = initHash(seed)
   result = result.mix(int32(data shr 32))
-  result = result.mixLast(int32(data and T(0xFFFF_FFFF)))
-  result = result.finalize(2)
+  result = result.mixLast(int32(data and T(0xFFFF_FFFF)), 1)
+  result = result.finalize()
 
-proc hash*[T: int8|int16|int32|uint8|uint16|uint32](data: T, seed: THash = 0): THash =
-  result = seed.mixLast(data)
-  result = result.finalize(1)
+proc hash*[T: int8|int16|int32|uint8|uint16|uint32](data: T, seed: int32): IntermediateHash =
+  result = initHash(seed)
+  result = result.mixLast(data, 1)
+  result = result.finalize()
 
-proc hash*(data: char, seed: THash = 0): THash =
-  result = seed.mixLast(ord(data))
-  result = result.finalize(1)
+proc hash*(data: char, seed: int32): IntermediateHash =
+  result = initHash(seed)
+  result = result.mixLast(ord(data), 1)
+  result = result.finalize()
 
-proc hash*(data: float32, seed: THash): THash =
+proc hash*(data: float32, seed: int32): IntermediateHash =
   hash(cast[int32](data), seed)
 
-proc hash*(data: float64, seed: THash): THash =
+proc hash*(data: float64, seed: int32): IntermediateHash =
   hash(cast[int64](data), seed)
 
 when isMainModule:
