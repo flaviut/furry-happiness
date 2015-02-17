3e4ababb6aa05c916795628ca6ec00588057ef5e
Flaviu Tamas <tamasflaviu@gmail.com>
2015-02-17 14:03:06 -0500

    Modularize chi-square testing

--- rand.nim
+++ rand.nim
@@ -28,26 +28,34 @@ proc next*[T](self: var T, n: int): int =
       assert result < n
       assert result >= 0
       break
-  
-import unittest, xorshift, strutils
-var state = seed([578247, 39337, 675246, 1567546723, 2390230, 5435346, 3476457, 960482304, 85948609, 659406839,
-                  86349034, 4356934, 65454365, 564356, 3458910, 0294])
-#for i in 0 .. 10000:
-#  discard state.next()
-#var buckets: array[0..999, int]
-#for i in 1..200000000:
-#  inc buckets[state.next(1000)]
-#
-#
-#var
-#  chiSquare = 0.0
-#  mean = 200_000
-#for i, elem in buckets:
-#  let part = ((elem-mean)*(elem-mean))/mean
-#  chiSquare += part
-#assert chiSquare < 1143.9169
-#echo chiSquare.int
 
-while true:
-  var nextInt = state.next
-  discard stdout.writeBuffer(addr nextInt, sizeof(nextInt))
+import unittest, xorshift, strutils, math
+
+proc chiSquare(fun: proc(): int, bucketCount, experiments: int):
+  tuple[chiSquare : float, buckets : seq[int]] =
+
+  var chiSquareVal = 0.0
+  var buckets = newSeq[int](bucketCount)
+  let mean = experiments / bucketCount
+
+  for i in 0..experiments:
+    buckets[int(fun())] += 1
+
+  for i, elem in buckets:
+    chiSquareVal += pow(float(elem) - mean, 2.0) / mean
+
+  return (chiSquareVal, buckets)
+
+
+var state = seed([58247, 39337, 675246, 1567546723, 2390230, 5435346, 3476457, 960482304, 85948609, 659406839,
+                  86349034, 4356934, 65454365, 564356, 3458910, 0294])
+
+for i in 0 .. 10000:
+  discard state.next()
+
+const
+  modulus = 1000
+  experiments = 200000000
+
+proc getRand(): int = state.next(modulus)
+echo chiSquare(getRand, modulus, experiments)
